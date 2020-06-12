70623d8b7159759977ba5e3c9c7bf1a127a1b27f
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-06-12 22:52:36 +0700

    responsive design, file splitting, small changes

--- index.js
+++ index.js
@@ -4,32 +4,29 @@ $(function(){
 
     if (this.checked) {
       $('body').removeClass('dark-mode-body');
-      $('body').addClass('light-mode-body'); 
-      $('.today-overview__section__overview-card').addClass('light-mode');
-      $('.social-media-card_facebook').addClass('light-mode');
-      $('.social-media-card_twitter').addClass('light-mode');
-      $('.today-overview__header').addClass('light-mode');
-      $('.social-media-card_instagram').addClass('light-mode');
-      $('.social-media-card_youtube').addClass('light-mode');
-      $('.main__heading__dark-mode__switch__toggle').addClass('light-mode');
-      $('.social-media-card__followers').addClass('light-mode');
-      $(`.social-media-card__profile, .overview-card__text, 
-      .main__followers, .main__heading__dark-mode__text`).addClass('light-mode');
-      $('.today-overview__section__overview-card').addClass('light-mode');
+      $('body').addClass('light-mode-body');
 
+      $('div').addClass('light-mode');
+      $('h1').addClass('light-mode');
+      $('h2').addClass('light-mode');
+      $('p').addClass('light-mode');
+      $('label').addClass('light-mode');
+      $('span').addClass('light-mode');
+      $('header').addClass('light-mode');
+      $('input').addClass('light-mode');
     } 
     else {
       $('body').removeClass('light-mode-body');
-      $('.social-media-card_facebook').removeClass('light-mode');
-      $('.social-media-card_twitter').removeClass('light-mode');
-      $('.social-media-card_instagram').removeClass('light-mode');
-      $('.today-overview__header').removeClass('light-mode');
-      $('.social-media-card_youtube').removeClass('light-mode');
-      $('.main__heading__dark-mode__switch__toggle').removeClass('light-mode');
-      $(`.social-media-card__profile, .overview-card__text, 
-      .main__followers, .main__heading__dark-mode__text`).removeClass('light-mode');
-      $('.social-media-card__followers').removeClass('light-mode');
-      $('.today-overview__section__overview-card').removeClass('light-mode');
+
+      $('div').removeClass('light-mode');
+      $('h1').removeClass('light-mode');
+      $('h2').removeClass('light-mode');
+      $('label').removeClass('light-mode');
+      $('p').removeClass('light-mode');
+      $('span').removeClass('light-mode');
+      $('header').removeClass('light-mode');
+      $('input').removeClass('light-mode');
+
       $('body').addClass('dark-mode-body');
     }
   });
